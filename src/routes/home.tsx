57c331229184ea376be19dc4e662d6152e2870c1
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-07-18 18:54:56 +0900

    ✨ Navigation Bar
    네비게이션바 구현

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,5 @@
-import { auth } from "../firebase";
-
 export default function Home() {
-  const logout = () => {
-    auth.signOut();
-  };
-
   return (
-    <h1>
-      <button onClick={logout}>로그아웃</button>
-    </h1>
+    <h1></h1>
   );
 }
